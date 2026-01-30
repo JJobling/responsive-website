62fc02b4270bf9679453854092395b42a781f907
John Jobling <66486909+JJobling@users.noreply.github.com>
2026-01-30 17:21:14 +0000

    Top Navigation - hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-console.log("hello world");
+const main = document.querySelector("#main");
+const footer = document.querySelector("#footer");
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const menuTopNav = document.querySelector("#menuTopNav");
+const breakpoint = window.matchMedia("(width < calc(600 / 16 * 1rem))");
+
+setupTopNav();
+
+if (breakpoint.matches) {
+  console.log("is mobile");
+  menuTopNav.setAttribute("inert", "");
+} else {
+  console.log("is tablet/desktop");
+  menuTopNav.removeAttribute("inert");
+}
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+breakpoint.addEventListener("change", () => {
+  console.log("breakpoint crossed");
+  setupTopNav();
+});
+
+function openMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "true");
+  menuTopNav.removeAttribute("inert");
+  main.setAttribute("inert", "");
+  footer.setAttribute("inert", "");
+  animateMenu();
+  bodyScrollLockUpgrade.disableBodyScroll(menuTopNav);
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "false");
+  menuTopNav.setAttribute("inert", "");
+  main.removeAttribute("inert", "");
+  footer.removeAttribute("inert", "");
+  animateMenu();
+  bodyScrollLockUpgrade.enableBodyScroll(menuTopNav);
+  btnOpen.focus();
+}
+
+function animateMenu() {
+  menuTopNav.classList.add("animating");
+
+  setTimeout(() => {
+    menuTopNav.classList.remove("animating");
+  }, 300);
+}
+
+function setupTopNav() {
+  if (breakpoint.matches) {
+    console.log("is mobile");
+    menuTopNav.setAttribute("inert", "");
+  } else {
+    console.log("is tablet/desktop");
+    closeMobileMenu();
+    menuTopNav.removeAttribute("inert");
+  }
+}
